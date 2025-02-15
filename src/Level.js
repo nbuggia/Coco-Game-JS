7efebfce72aebe584ca2d816b9d49eba941d8451
Nathan Buggia <594357+nbuggia@users.noreply.github.com>
2025-02-15 15:03:31 -0800

    updates

--- src/Level.js
+++ src/Level.js
@@ -1,5 +1,20 @@
 export class Level {
-    run() {
-        console.log('running')
-    }
+  constructor() {
+    this.setup();
+  }
+
+  setup = async () => {
+    this.canvas = document.querySelector("canvas");
+  };
+
+  run() {
+    console.log("running");
+  }
+
+  setScreenSize() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    this.canvas.width = w;
+    this.canvas.height = h;
+  }
 }
--- src/Settings.js
+++ src/Settings.js
@@ -1,5 +1,25 @@
 export class Settings {
-    static #property = {};
+  static #properties = {};
 
-    //static get(property)
+  static get(p) {
+    return this.#properties[p];
+  }
+
+  static add(p, value) {
+    if (!Object.getOwnPropertyDescriptor(this)[p]) {
+      Object.defineProperty(this, p, {
+        configurable: true,
+        enumerable: true,
+        get: () => this.#properties[p],
+        set: (v) => {
+          this.#properties[p] = v;
+        },
+      });
+    }
+    this.#properties[p] = value;
+  }
+
+  static remove(p) {
+    delete this.#properties[p];
+  }
 }
